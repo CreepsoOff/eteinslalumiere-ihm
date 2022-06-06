618ac301fc4998e81f229ecd196854977df88d07
Fabian Gelin <fabian.gelin9@etu.univ-lorraine.fr>
2022-06-06 14:54:47 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module fr.fabian.eteinslalumiereihm {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+                            
+    opens fr.fabian.eteinslalumiereihm to javafx.fxml;
+    exports fr.fabian.eteinslalumiereihm;
+}
