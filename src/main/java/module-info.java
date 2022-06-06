b0be4488096cb744bd40639ab2e70cb3febd4269
Fabian Gelin <fabian.gelin9@etu.univ-lorraine.fr>
2022-06-06 14:57:53 +0200

    Correction .gitignore

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module fr.fabian.eteinslalumiereihm {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-                            
+
+    requires org.controlsfx.controls;
+
     opens fr.fabian.eteinslalumiereihm to javafx.fxml;
     exports fr.fabian.eteinslalumiereihm;
 }
